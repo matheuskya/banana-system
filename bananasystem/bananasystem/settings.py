f95541361c0c4c053f995772bd7ab2227454e8fd
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2024-10-07 15:18:31 -0300

    security update

--- bananasystem/bananasystem/settings.py
+++ bananasystem/bananasystem/settings.py
@@ -3,19 +3,24 @@ from pathlib import Path
 import os
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i9(7q$e)r$r7b^!$3vk74_b7v)!@4&$bw#f978jho+0e_l38bv'
+# SECRET_KEY = 'django-insecure-i9(7q$e)r$r7b^!$3vk74_b7v)!@4&$bw#f978jho+0e_l38bv'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8_a+-$4w1f^lt3lx1c@d*p4x$ymm_rn7pwb87')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
