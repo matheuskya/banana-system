02094c087e9c6cfb1348b69152deec87546c67f4
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2024-10-08 11:27:06 -0300

    Update settings.py

--- bananasystem/bananasystem/settings.py
+++ bananasystem/bananasystem/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
+
 ]
 
 ROOT_URLCONF = 'bananasystem.urls'
